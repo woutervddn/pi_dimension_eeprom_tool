c7fef9e161c73ac0d0b8a7b42d7054bc4138d697
Wouter Vandenneucker <wouter@vandenneucker.be>
2017-01-15 13:41:23 +0100

    added make abs

--- web/make-abs-eeprom.php
+++ web/make-abs-eeprom.php
@@ -18,33 +18,13 @@
 <div class="container">
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
+      <div class="well well-lg" style="font-family:monospace;">
         <?php
-	if( $_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['centimeter-amount']) ){
-	        // outputs the username that owns the running php/httpd process
-	        // (on a system with the "whoami" executable in the path)
-		$diameter = 0.175; //diameter in centimeter
-		$quantity_centi = round ( $_POST['centimeter-amount'] * ( 3.14159265359 * ($diameter/2) * ($diameter/2) ), 1); //Material in centimeter ^ 3
-		$quantity = $quantity_centi * 0.061024; //Material in inch ^3
-	      echo '<div class="well well-lg" style="font-family:monospace;">';
-	        echo nl2br(shell_exec('sudo ' . dirname(__FILE__) . '/scripts/custom-eeprom.sh ' . $quantity));
-	      echo '</div>';
-	} else {
-		echo '<div class="spacer" style="height: 30px"></div>';
-		echo '<form method="POST" class="form-inline">
-			<div class="form-group">
-				<label for="centimeter-amount">New material length: </label>
-				<div class="input-group">
-					<input type="text" class="form-control" id="centimeter-amount" name="centimeter-amount" placeholder="Length">
-					<div class="input-group-addon">centimeter</div>
-				</div>
-			</div>
-			<button type="submit" class="btn btn-primary">Write to EEPROM</button>
-      <a href="reset.php" class="btn btn-default">Reset material length</a>
-		</form>';
-		echo '<div class="spacer" style="height: 30px"></div>';
-    echo '<div class="row"><div class="col-md-6 col-md-offset-3"><a href="make-abs-eeprom.php" class="btn btn-block btn-warning">Change material to ABS</a></div></div>';
-	}
+        // outputs the username that owns the running php/httpd process
+        // (on a system with the "whoami" executable in the path)
+        echo nl2br(shell_exec('sudo ' . dirname(__FILE__) . '/scripts/make-abs-eeprom.sh'));
         ?>
+      </div>
     </div>
   </div>
 </div>
